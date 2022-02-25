6389a4357da04f129142af174a3c340edb23ca6d
NotTracy <100299623+NotTracy@users.noreply.github.com>
2022-02-24 21:15:02 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
-let strike = 0
-let ball = 0
 input.onButtonPressed(Button.A, function () {
     strike += 1
+    basic.pause(500)
+    basic.showString("S")
     basic.showNumber(strike)
+    if (strike == 3) {
+        basic.clearScreen()
+        basic.showString("OUT")
+    }
 })
 input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
     basic.showNumber(ball)
     basic.showNumber(strike)
     strike = 0
@@ -12,8 +17,18 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     ball += 1
+    basic.pause(500)
+    basic.showString("B")
     basic.showNumber(ball)
+    if (ball == 4) {
+        basic.clearScreen()
+        basic.showString("WALK")
+    }
 })
+let ball = 0
+let strike = 0
+basic.showIcon(IconNames.Duck)
+basic.clearScreen()
 basic.forever(function () {
 	
 })
